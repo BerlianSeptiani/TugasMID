3f0a89b2c8a3f58db924159de41b1819650d0d32
BerlianSeptiani <127605592+BerlianSeptiani@users.noreply.github.com>
2023-03-30 10:45:38 +0800

    Add files via upload

---
+++ TestErr0143.java
@@ -0,0 +1,31 @@
+/* Nama : Berlian Septiani
+   Nim  : 13020210143
+   Kelas : B2 */
+
+
+
+import java.util.Scanner;
+
+
+ 
+public class TestErr0143 {
+    
+    public static void main(String[] args) {
+ 
+     
+       Scanner input = new Scanner(System.in);
+ 
+       int i = input.nextInt();
+ 
+       int k = 100;
+
+       char c = 's';
+ 
+       boolean falses = true;
+
+       int j = i + 1;
+       System.out.println("j adalah " + j + "k adalah " + k + "dan c adalah " + c + ", dan trues adalah " + falses);
+    
+      }
+
+}
