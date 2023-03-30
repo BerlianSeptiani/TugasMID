ccf1749545e630315ccadece3b9719b7084e57e7
BerlianSeptiani <127605592+BerlianSeptiani@users.noreply.github.com>
2023-03-30 11:00:29 +0800

    Add files via upload

---
+++ Central0143.java
@@ -0,0 +1,44 @@
+/* Nama : Berlian Septiani
+   Nim  : 13020210143
+   Hari/Tanggal	:Rabu, 30 Maret 2023
+   Kelas : B2 */
+
+
+
+
+interface A {
+ 
+   public void aaa();
+
+  }
+
+
+
+  interface B extends A {
+
+
+    
+}
+
+
+class Central0143 implements B {
+ 
+   public void aaa() {
+  
+      System.out.println("aaa");
+   
+ }
+
+ 
+
+   public static void main(String arg[]) {
+ 
+       Central0143 obj = new Central0143();
+ 
+       System.out.println("main");
+
+        obj.aaa();
+    
+   }
+
+}
